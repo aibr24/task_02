c25504567e8f5d7aa757957e97295e2e21c94250
ali <aliibrahim4793@gmail.com>
2019-07-10 20:51:17 +0300

    task 2 commit

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mi_function(request):
+	HW = "Hello World!"
+	context = { 
+	"msg" : HW,
+	}
+	return render(request, "index.html", context)
--- task_02/urls.py
+++ task_02/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from restaurants import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('anything/', views.mi_function, name='hello-world'),
 ]
